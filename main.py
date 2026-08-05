8ed08c541948ec16c28a4687753ee1f91b81cf53
buccobruce <dcmcmillan@gmail.com>
2026-08-05 08:41:05 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import rich
+
+
+def target_select():
+    print("SELECT YOUR TARGET: ")
+    print("1. NORAD MISSILE COMMAND")
+    print("2. CENTRAL INTELLIGENCE AGENCY")
+    print("3. DEPARTMENT OF DEFENSE")
+    print("4. INTERNAL REVENUE SERVICE")
+    choice = int(input())
+    choices = [1, 2, 3, 4]
+    while choice not in choices:
+        print("Incorrect choice, please try again...")
+        choice = int(input())
+
+
+def main():
+    target_select()
+
+
+main()
