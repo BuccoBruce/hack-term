264cb97df4ab9ecfc83518d60427b407bde473fc
Chris McMillan <dcmcmillan@gmail.com>
2026-08-05 10:32:11 -0700

    Added NORAD screen

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import rich
 
+def get_input(choices):
+    choice = input()
+    while choice not in choices:
+        print("Incorrect choice, please try again...")
+        choice = input()
+    return choice
 
-def target_select():
+def main_screen():
     print("SELECT YOUR TARGET: ")
     print("1. NORAD MISSILE COMMAND")
     print("2. CENTRAL INTELLIGENCE AGENCY")
     print("3. DEPARTMENT OF DEFENSE")
     print("4. INTERNAL REVENUE SERVICE")
-    choice = input()
     choices = ["1", "2", "3", "4"]
-    while choice not in choices:
-        print("Incorrect choice, please try again...")
-        choice = input()
+    choice = get_input(choices)
+    if choice == "1":
+        norad_screen()
+    return choice
 
+def norad_screen():
+    print("NORAD COMMAND CENTER")
+    print("FOR AUTHORIZED USERS ONLY")
+    choices = ["launch missile", "exit"]
+    choice = get_input(choices)
+    if choice == "launch missile":
+            print("MISSILE LAUNCHED")
+            norad_screen()
+    if choice == "exit":
+        main_screen()
 
 def main():
-    target_select()
-
+    main_screen()
 
 main()
